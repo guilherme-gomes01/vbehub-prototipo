b6720042cb995430e26c3ac29cbba17802bcc87a
guilherme-gomes01 <guilhermejgsg01@gmail.com>
2026-02-01 17:10:26 -0400

    fix: cors configuration

--- api/src/main/java/com/vbehub/api/config/SecurityConfig.java
+++ api/src/main/java/com/vbehub/api/config/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
